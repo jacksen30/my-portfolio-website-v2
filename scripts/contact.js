d7daeff42c2518dac3ddba41cf7d127800d15aaf
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-12-11 12:37:04 +0800

    update contact form logic remove clearing of placeholders and values form input upon failed validation to improve user experience

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,4 @@
-// Selecting these divs by .getElementById to ensure that they are unique and not clasases that may be used on multiple elements 
+// Selecting these divs by .getElementById to ensure that they are unique and not clasases that may be used on multiple elements
 const form = document.getElementById('contact-form');
 const fullName = document.getElementById('fname-wrapper').children;
 const email = document.getElementById('email-wrapper').children;
@@ -11,27 +11,21 @@ function validateInput(item) {
     /* item[1] is the child of item at index position [1]. The input tag */
     /* item[2] is the child of item at index position [2]. The paragraph element with the class error-message */
 
-    if(item[1].value.length === 0){
+    if(item[1].value.length === 0) {
         item[1].classList.add('incorrect');
-        item[1].placeholder="";
-        item[1].value="";
         item[2].classList.remove('none');
     } else {
         item[1].classList.remove('incorrect');
-        item[1].placeholder="";
         item[2].classList.add('none');
     }
 }
 
 function validateEmail(item) {
-
     const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const emailValid = regexEmail.test(item[1].value);
 
     if(item[1].value.length === 0 || !emailValid){
         item[1].classList.add('incorrect');
-        item[1].placeholder="email@example.com";
-        item[1].value="";
         item[2].classList.remove('none');
     } else {
         item[1].classList.remove('incorrect');
@@ -40,21 +34,15 @@ function validateEmail(item) {
 }
 
 function validateMessage(item) {
-
     // Currently this function is the same as validateInput(), except it requires an input of atleast 80 characters
-
     if(item[1].value.length < 80){
         item[1].classList.add('incorrect');
-        item[1].placeholder="";
-        item[1].value="";
         item[2].classList.remove('none');
     } else {
         item[1].classList.remove('incorrect');
-        item[1].placeholder="";
         item[2].classList.add('none');
     }
 }
-
 
 function validate(){
     validateInput(fullName);
@@ -90,9 +78,3 @@ form.addEventListener('submit', (event) => {
           });
       }
     });
-
-    /* Not Required as error / text hint warning text will appear below inputs that haven't passed validation */
-    //   else {
-    //     alert('Please fill in all required fields correctly before submitting.');
-    //   }
-    // });
